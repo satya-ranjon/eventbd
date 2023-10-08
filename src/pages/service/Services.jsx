571afbfe238e1338bc1c8342cea84ac777397853
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-10-08 12:37:37 +0600

    feat: Add Services component

---
+++ src/pages/service/Services.jsx
@@ -0,0 +1,11 @@
+import AllServices from "../../components/AllServices";
+
+const Services = () => {
+  return (
+    <div className=" container mx-auto px-5 2xl:px-0 mt-10">
+      <AllServices label="Our All Services" />
+    </div>
+  );
+};
+
+export default Services;
